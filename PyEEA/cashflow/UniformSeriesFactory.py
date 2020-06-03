445f52d411f027fe4af0731e018380f0dbc7e777
Thomas Richmond <thomas.joakim@gmail.com>
2020-06-02 18:57:31 -0700

    Added stubs for Uniform Series

--- PyEEA/cashflow/UniformSeriesFactory.py
+++ PyEEA/cashflow/UniformSeriesFactory.py
@@ -1,8 +1,18 @@
 from .Cashflow import Cashflow
 
-
-def Annuity(Cashflow):
+class Annuity(Cashflow):
     pass
 
+class Gradient(Cashflow):
+    pass
+
+class Geometric(Cashflow):
+    pass
+
+class Perpetuity(Cashflow):
+    pass
+
+class GPerpetuity(Cashflow):
+    pass
 
 # TODO add more stuff...
