18cb5f37e780e06b4efe4427db948fea4d768f4f
Thomas Richmond <thomas.joakim@gmail.com>
2020-06-08 13:23:43 -0700

    Added a cooler example!

--- example.py
+++ example.py
@@ -3,10 +3,13 @@ from PyEEA import SinglePaymentFactory as sp
 
 my_project = Project(interest=0.12)
 
-my_project                               \
-    .add_cashflow(sp.Present(-1200))     \
-    .add_cashflow( sp.Future( 100,  1))  \
-    .add_cashflow( sp.Future( 200,  3))  \
-    .add_cashflow( sp.Future( 1200, 5))
+my_project                                \
+    .add_cashflow( sp.Future(-1000,  1))  \
+    .add_cashflow( sp.Future(-600,  2))   \
+    .add_cashflow( sp.Future(-500,  3))   \
+    .add_cashflow( sp.Future(-100, 5))    \
+    .add_cashflow( sp.Future( 5000, 5))
 
-print("Net Present Worth of Project: $%.2f" % my_project.npw())
+print("Equivalent Uniform Cashflow of Project:", my_project.eucf(1))
+print("IRR:", my_project.irr())
+print("MIRR:", my_project.mirr())
