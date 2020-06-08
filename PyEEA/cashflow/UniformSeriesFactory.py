b692ee2c3b96a2684e0ebc3734f0f249af23dc94
Thomas Richmond <thomas.joakim@gmail.com>
2020-06-08 13:14:54 -0700

    Added methods to get cashflows at a specified period; fixed some bugs

--- PyEEA/cashflow/UniformSeriesFactory.py
+++ PyEEA/cashflow/UniformSeriesFactory.py
@@ -43,30 +43,41 @@ class Annuity(Cashflow):
         
         if type(d) is list:
             if len(d) == 1 and isint(d[0]):
-                return [1, int(d[0])]
+                return [0, int(d[0])]
             elif len(d) == 2 and isint(d[0]) and isint(d[1]):
                 return [int(d[0]), int(d[1])]
             else:
                 raise ValueError("Argument n in list form must contain whole numbers," + \
                                  "and its length must not exceed 2")
         elif isint(d):
-            return [1, int(d)]
+            return [0, int(d)]
         else:
             raise TypeError("Type of argument n <%s> is not supported;" % type(d) + \
                             "must be whole number or list of whole numbers")
 
+    def cf_at(self, n):
+        if n in range(self.d[0], self.d[1] + 1):
+            if n == 0:
+                return sp.Present(0)
+            else:
+                return sp.Future(self.amount, n)
+        else:
+            if n == 0:
+                return sp.Present(0)
+            else:
+                return sp.Future(0, n)
 
     def to_pv(self, i):
         pv = self.amount * ((1 + i)**self.D - 1) / (i * (1+i)**self.D)
 
-        if self.d[0] == 1:  # PV is correct
+        if self.d[0] == 0:  # PV is correct
             return sp.Present(pv)
         else:  # If our annuity begins in the future, v is a FV and must
                # be converted to a PV.
             return sp.Future(pv, self.d[0]).to_pv(i)
 
     def to_fv(self, i, n):
-        if self.d[0] == 1:  # Use standard formulas
+        if self.d[0] == 0:  # Use standard formulas
             if self.d[1] == n:
                 fv = self.amount * ((1 + i)**n - 1) / i
                 return sp.Future(fv, n)
@@ -92,12 +103,26 @@ class Gradient(Annuity):
     def __init__(self, amount, d, G):
         super().__init__(amount, d)
         self.G = G
+    
+    def cf_at_period(self, n):
+        if n in range(self.d[0], self.d[1] + 1):
+            if n == 0:
+                return sp.Present(self.amount)
+            else:
+                fv = self.amount + self.G * (n - self.d[0])
+                return sp.Future(fv, n)
+        else:
+            if n == 0:
+                return sp.Present(0)
+            else:
+                return sp.Future(0, n)
+
 
     def to_pv(self, i):
         pv1 = self.amount * ((1 + i)**self.D - 1) / (i * (1+i)**self.D)  # Annuity Term
         pv2 = self.G * ((1 + i)**self.D - i * self.D - 1) / (i**2 * (1 + i) ** self.D)  # Gradient Term
         pv = pv1 + pv2
-        if self.d[0] == 1:  # Requested gradient is equivalet to this instance
+        if self.d[0] == 0:  # Requested gradient is equivalet to this instance
             return sp.Present(pv)
         else:  # The gradient starts at n > 0, so we need to convert a "future present value" to a present value
             return sp.Future(pv, d[0]).to_pv(i)
@@ -109,7 +134,7 @@ class Gradient(Annuity):
         d = self.parse_d(d)
         D = d[1] - d[0] + 1
         
-        if d == self.d and d[0] == 1:  # Use standard formula
+        if d == self.d and d[0] == 0:  # Use standard formula
             A_eq = G * (1/i - D / ((1 + i) ** n - 1))
             return Annuity(A_eq)
         else:
@@ -120,18 +145,32 @@ class Geometric(Annuity):
         super().__init__(amount, d)
         self.g = g
     
+    def cf_at_period(self, n):
+        if n in range(self.d[0], self.d[1] + 1):
+            if n == 0:
+                return sp.Present(self.amount)
+            else:
+                fv = self.amount * (1 + g)**(n - self.d[0])
+                return sp.Future(fv, n)
+        else:
+            if n == 0:
+                return sp.Present(0)
+            else:
+                return sp.Future(0, n)
+
+
     def to_pv(self, i):
         if i == self.g:
             xv = self.amount * self.D * (1 + i) ** -1
             
-            if self.d[0] == 1:
+            if self.d[0] == 0:
                 return sp.Present(xv)
             else:
                 return sp.Future(xv, self.d[0]).to_pv(i)
         elif i > self.g:
             xv = self.amount * (1 - (1 + self.g) ** self.D * (1 + self.g) ** -self.D) / (i - self.g)
 
-            if self.d[0] == 1:
+            if self.d[0] == 0:
                 return sp.Present(xv)
             else:
                 return sp.Future(xv, self.d[0]).to_pv(i)
@@ -156,6 +195,10 @@ class Geometric(Annuity):
         return self.to_pv(i).to_av(i, d)
 
 class Perpetuity(Cashflow):
+
+    def cf_at(self, n):
+        return sp.Future(self.amount, n)
+
     def to_pv(self, i):
         return sp.Present(self.amount / i)
     
