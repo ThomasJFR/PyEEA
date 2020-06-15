2fadfca1ff5e10f04c8d4daa6ef2dc1d152d2a18
Thomas Richmond <thomas.joakim@gmail.com>
2020-06-14 23:27:39 -0700

    Added header support

--- PyEEA/output/spreadsheet.py
+++ PyEEA/output/spreadsheet.py
@@ -24,18 +24,30 @@ def to_excel(filename, project, show_npv=False):
 
     titles = ["Period"] + [cf.get_title() for cf in project.get_cashflows()]
     with xlsxwriter.Workbook(filename) as wb:
-        ws = wb.add_worksheet()
         bld = wb.add_format({'bold': True})
+        pct = wb.add_format({'num_format': '0.00%'})
         fin = wb.add_format({'num_format': '_-$* #,##0.00_-;[Red]-$* #,##0.00_-;_-$* "-"??_-;_-@_-'})
+
+        ws = wb.add_worksheet()
         
+        row = 0
+        
+        # HEADER
+        ws.write(row, 0, project.title, bld)
+        row += 1
+
+        ws.write(row, 0, "Interest", bld)
+        ws.write(row, 1, project.interest, pct)
+        row += 1
+
         # TITLES
         for i in range(len(titles)):
-            ws.write(0, i, titles[i], bld)
+            ws.write(row, i, titles[i], bld)
         if show_npv:
-            ws.write(0, len(titles), "Net Present Worth", bld)
+            ws.write(row, len(titles), "Net Present Worth", bld)
+        row += 1
 
         # DATA
-        row = 1
         for n in range(project.periods + 1): 
             ws.write(row, 0, n)
             for cf in project[n]:
