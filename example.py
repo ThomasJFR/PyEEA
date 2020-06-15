b4b8853d6b4ac71f23ea4dc392b00e092071c1d2
Thomas Richmond <thomas.joakim@gmail.com>
2020-06-14 23:13:06 -0700

    Added XLSX writing to example

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 from PyEEA import Project
 from PyEEA import SinglePaymentFactory as sp
 from PyEEA import UniformSeriesFactory as us
+from PyEEA.output import to_excel
 
 print("CASHFLOW STUFF")
 print("--------------")
@@ -25,4 +26,5 @@ print("Equivalent Uniform Cashflow of Project:", my_project.eucf(5))
 print("Benefit-to-Cost Ratio:", my_project.bcr())
 print("IRR:", my_project.irr())
 print("MIRR:", my_project.mirr())
+to_excel("test.xlsx", my_project, show_npv=True)
 
