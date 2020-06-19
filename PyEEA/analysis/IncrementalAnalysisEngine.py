8669f18ff1f72b5b72e05ac68a0b4b94acf31aed
Thomas Richmond <thomas.joakim@gmail.com>
2020-06-19 13:00:40 -0700

    Fixed an iterator noncompliance issue within the Incremental Analysis function

--- PyEEA/analysis/IncrementalAnalysisEngine.py
+++ PyEEA/analysis/IncrementalAnalysisEngine.py
@@ -9,16 +9,18 @@ def dirr_analysis(projects, marr):
     sorted_projects = iter(sorted(projects, key=lambda p: -p.npw().amount))
 
     # Phase 1: Find a defender
-    while defender := next(sorted_projects):
-        if defender.irr() > marr:
-            break
-    else:
-        return None  # Yikes - no project is worth doing!
+    defender = None
+    try:
+        while candidate := next(sorted_projects):
+            if candidate.irr() > marr:
+                defender = candidate
+                break
 
-    # Phase 2: Perform Incremental IRR Analysis
-    while challenger := next(sorted_projects):
-        inc = lambda i: challenger.npw(i) - defender.npw(i)
-        dirr = fsolve(inc, challenger.interest)  # Not guaranteed to work!
-        if dirr > marr:
-            defender = challenger
-    return challenger
+        # Phase 2: Perform Incremental IRR Analysis
+        while challenger := next(sorted_projects):
+            inc = lambda i: challenger.npw(i) - defender.npw(i)
+            dirr = fsolve(inc, challenger.interest)  # Not guaranteed to work!
+            if dirr > marr:
+                defender = challenger
+    except StopIteration:
+        return defender
