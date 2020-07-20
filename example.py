25cd1734f4f32600996c0a418eb8f7b167c0d2bf
Thomas Richmond <thomas.joakim@gmail.com>
2020-07-20 22:15:14 +0200

    Updated example with new stuff

--- example.py
+++ example.py
@@ -1,15 +1,18 @@
 from PyEEA import Project
 from PyEEA import SinglePaymentFactory as sp
 from PyEEA import UniformSeriesFactory as us
+from PyEEA import DepreciationHelper as dh
 from PyEEA.output import write_excel, SpreadsheetFeature as ssfs
 
 print("CASHFLOW STUFF")
 print("--------------")
-gp = us.GeoPerpetuity(1200, 0.15, 1)
-print(gp)
-print(gp[0])
-print([str(cf) for cf in gp[1, 5]])
-print([str(cf) for cf in gp[1:3]])
+
+depreciator = dh.DoubleDecliningBalance(0.20, 10, title="Big Boy", cashflows=[
+    sp.Present(-100000),
+], salvage=10000)
+
+for i in range(12):
+    print(depreciator[i])
 
 print("PROJECT STUFF")
 print("-------------")
@@ -23,6 +26,7 @@ my_project                                                              \
     .add_cashflow(us.Perpetuity(50, 2,      title="Lottery Win"))       \
     .add_cashflow(us.GeoPerpetuity(10, 0.1, title="Yearly Bonus"))
 
+"""
 print([[str(cf) for cf in p] for p in my_project[:]])
 print("Net Present Worth:", my_project.npw())
 print("Net Future Worth at n=5:", my_project.nfw(5))
@@ -31,12 +35,4 @@ print("Benefit-to-Cost Ratio:", my_project.bcr())
 print("IRR:", my_project.irr())
 print("MIRR:", my_project.mirr())
 write_excel("test.xlsx", my_project, [ssfs.NPW, ssfs.CNPW])
-
-""" QUIZ 
-p = Project(interest=0.10)
-p.add_cashflows([
-    sp.Present(-1000),
-    us.Annuity( 500, 5)
-])
-print(p.bcr())
 """
