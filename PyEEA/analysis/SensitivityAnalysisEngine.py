315668cb5900a1f0de641ba1693c52583c745e3c
TZYR (Thomas Richmond) <TZYR@novonordisk.com>
2020-08-03 15:40:44 +0200

    Updated to work with new tagging system

--- PyEEA/analysis/SensitivityAnalysisEngine.py
+++ PyEEA/analysis/SensitivityAnalysisEngine.py
@@ -1,19 +1,31 @@
 
-def sensitivity_analysis(project, factors, cf_titles=None, valuator=None):
+def sensitivity_analysis(project, factors, cf_tags=None, valuator=None):
     valuator = valuator or project.npw
-    cf_titles = cf_titles or [cf.get_title() for cf in project.cashflows]
+    cf_tags = cf_tags or [cf.get_title() for cf in project.get_cashflows()]
     if not callable(valuator):
         return TypeError("Valuator must be a callable construct!")
 
     all_valuations = {}
-    for title in cf_titles:
-        valuations = []
-        for factor in factors:
-            with project as p:
-                p[title].amount *= factor
-                valuations.append(valuator())
-    
-        all_valuations[title] = valuations
+    valuated_cashflows = set()  # Prevents repeats
+    for tag in cf_tags:
+        n_cashflows = len(project[tag])
+        if n_cashflows == 0:
+            continue
+
+        for i in range(n_cashflows):
+            # Make sure we don't repeat any valuations
+            if project[tag][i].get_title() in valuated_cashflows:
+                continue
+            valuated_cashflows.update(project[tag][i].get_title())
+
+            valuations = []
+            for factor in factors:
+                with project as p:
+                    
+                    p[tag][i].amount *= factor
+                    valuations.append(valuator())
+
+            all_valuations[p[tag][i].get_title()] = valuations
     
 
     return all_valuations
