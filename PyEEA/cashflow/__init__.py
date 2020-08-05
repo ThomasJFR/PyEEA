afbd5b47f49422aeda5738d21ba5d2589f7a6070
TZYR (Thomas Richmond) <TZYR@novonordisk.com>
2020-08-05 12:17:25 +0200

    Added new definitions for TaxationHelper and missing UniformSeriesFactory imports

--- PyEEA/cashflow/__init__.py
+++ PyEEA/cashflow/__init__.py
@@ -2,5 +2,6 @@
 from .Cashflow import Cashflow, NullCashflow, PaymentScheme
 
 from .SinglePaymentFactory import Present, Future
-from .UniformSeriesFactory import Annuity
-from .DepreciationHelper import StraightLine, SumOfYearsDigits, DoubleDecliningBalance
+from .UniformSeriesFactory import Annuity, Gradient, Geometric, Perpetuity, GeoPerpetuity
+from .DepreciationHelper import StraightLine, SumOfYearsDigits, DecliningBalance
+from .TaxationHelper import Tax
