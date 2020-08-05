761a74beae257e90713b9b636071d2890dcb2af4
Thomas Richmond <thomas.joakim@gmail.com>
2020-08-05 21:47:59 +0200

    Fixed a sign

--- PyEEA/taxation/TaxationHelper.py
+++ PyEEA/taxation/TaxationHelper.py
@@ -44,7 +44,7 @@ class Tax:
                     for depreciation in shielding_depreciations
                 ])
 
-                taxed_amount = (taxable_sum.amount - shielding_sum.amount) * self._rate
+                taxed_amount = (taxable_sum.amount + shielding_sum.amount) * self._rate
                 taxes.append(Future(taxed_amount, n))
             
             return taxes
