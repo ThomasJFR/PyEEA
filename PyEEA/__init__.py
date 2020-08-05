c9375f285cda42d790a86b3f9ccd11ecf40f2eef
TZYR (Thomas Richmond) <TZYR@novonordisk.com>
2020-08-05 12:18:53 +0200

    Added new definitions to init file

--- PyEEA/__init__.py
+++ PyEEA/__init__.py
@@ -5,6 +5,7 @@ from .cashflow import Cashflow
 from .cashflow import SinglePaymentFactory
 from .cashflow import UniformSeriesFactory
 from .cashflow import DepreciationHelper
+from .cashflow import TaxationHelper
 
 from .analysis import simulation_analysis, sensitivity_analysis
 
