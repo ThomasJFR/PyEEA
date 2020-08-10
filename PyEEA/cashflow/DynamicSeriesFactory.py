8364083a6f9d4d16c1b807d521f50f994c6c4fc0
TZYR (Thomas Richmond) <TZYR@novonordisk.com>
2020-08-10 15:27:03 +0200

    Removed some local access hints; Added temporary workaround for shorthand issues

--- PyEEA/cashflow/DynamicSeriesFactory.py
+++ PyEEA/cashflow/DynamicSeriesFactory.py
@@ -5,12 +5,12 @@ class Dynamic(Cashflow):
     def __init__(self, amount_fun, d, title=None, tags=None):
         super().__init__(0, title, tags)
         self._amount_fun = amount_fun
-        self._d = parse_d(d)
-        self._D = self._d[1] - self._d[0]
+        self.d = parse_d(d)
+        self.D = self.d[1] - self.d[0]
 
     def to_shorthand(self):
         # TODO New implementaton needed
-        return super().to_shorthand()
+        return "Dynamic Series"
     
     def cashflow_at(self, ns):
         ns = parse_ns(ns)
@@ -20,7 +20,7 @@ class Dynamic(Cashflow):
         return cashflows[0] if len(cashflows) == 1 else cashflows
 
     def to_pv(self, i):
-        return sum([self[n].to_pv(i) for n in range(self._d[0], self._d[1] + 1)])
+        return sum([self[n].to_pv(i) for n in range(self.d[0], self.d[1] + 1)])
 
     def to_fv(self, i, n):
         return self.to_pv(i).to_fv(i, n)
