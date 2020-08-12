8aac9461e0305d8ff20088eb815776d779c3043e
Thomas Richmond <thomas.joakim@gmail.com>
2020-08-12 22:29:07 +0200

    Added more initial implementation

--- PyEEA/analysis/WhatIfAnalysis.py
+++ PyEEA/analysis/WhatIfAnalysis.py
@@ -1,24 +1,24 @@
 from copy import deepcopy
 from collections.abc import Iterable
 
+from ..valuators import npw, nfw, eacf, bcr, irr, mirr
+
 class WhatIfAnalysis:
     def __init__(self, project):
         self._project = deepcopy(project)
 
-    def get_project(self):
-        self._project
-
-    def analyse(self, cashflows, valuator):
+    def analyse(self, cashflows, valuator=npw):
         if not isinstance(cashflows, Iterable):
             cashflows = [cashflows]
-        
         self._project.add_cashflows(cashflows)
-        
 
-    def to_dataframe():
+    def get_project():
+        return self._project
+
+    def get_dataframe():
         pass
 
-    def to_chart():
+    def get_chart():
         pass
 
 
