2dd8fbcc7c36849ba0d1eb5134bf4c49aa0c02cb
TZYR (Thomas Richmond) <TZYR@novonordisk.com>
2020-08-13 13:23:32 +0200

    Added missing imports; Fixed type hint syntax; Fixed net_cashflows implementation

--- PyEEA/valuation/Valuators.py
+++ PyEEA/valuation/Valuators.py
@@ -1,34 +1,33 @@
-from ..cashflows import NullCashflow
+from ..cashflow import NullCashflow, Present, Future, Annuity
 from ..utilities import parse_d, get_last_period
 
-def npw(cashflows, i, title=None): -> Present
+def npw(cashflows, i, title=None) -> Present:
     npw = sum([cf.to_pv(i) for cf in cashflows]) or NullCashflow()
     npw.set_title(title or "Net Present Worth")
     return npw
 
-def nfw(cashflows, i, n, title=None): -> Future
+def nfw(cashflows, i, n, title=None) -> Future:
     nfw = sum([cf.to_fv(i, n) for cf in cashflows]) or NullCashflow()
     npw.set_title(title or f"Net Future Worth")
     return nfw
 
-def eacf(cashflows, i, d, title=None): -> Annuity
+def eacf(cashflows, i, d, title=None) -> Annuity:
     d = parse_d(d)
     eacf = sum([cf.to_av(i, d) for cf in cashflows]) or NullCashflow()
     eacf.set_title(title or "Equivalent Annual Cashflow")
     return eacf
 
-def bcr(cashflows):
+def bcr(cashflows) -> float:
     nf = get_last_period(cashflows)
-    net_cashflows = [(sum(cfs) or NullCashflow()) for cfs in cashflows[:nf]]
-    nf = get_last_period(cashflows)
+    net_cashflows = [sum([cf[n] for cf in cashflows]) for n in range(nf+1)]
     rvnus = sum([ncf.to_pv(0) for ncf in net_cashflows if ncf > 0])
     costs = sum([ncf.to_pv(0) for ncf in net_cashflows if ncf < 0])
-    if all([pv_rvnus, pv_costs]):
+    if all([rvnus, costs]):
         return -(rvnus.amount / costs.amount)
     else:
         return None
 
-def irr(cashflows, i0=0): -> float
+def irr(cashflows, i0=0) -> float:
     from scipy.optimize import fsolve
     
     # IRR only exists if BCR exists
@@ -44,9 +43,9 @@ def irr(cashflows, i0=0): -> float
     else:
         return None
 
-def mirr(cashflows, e_inv, e_fin, i0=0): -> float
+def mirr(cashflows, e_inv, e_fin, i0=0) -> float:
     nf = get_last_period(cashflows)
-    net_cashflows = [(sum(cfs) or NullCashflow()) for cfs in cashflows[:nf]]
+    net_cashflows = [sum([cf[n] for cf in cashflows]) for n in range(nf+1)]
     fv_rvnu = sum([ncf.to_fv(e_fin, nf) for ncf in net_cashflows if ncf > 0])
     pv_cost = sum([ncf.to_pv(e_inv)     for ncf in net_cashflows if ncf < 0])
     return irr([pv_cost, fv_rvnu], i0)
